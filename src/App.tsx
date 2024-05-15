a725f9ea4ed09a01a65299d978f70bab94b0e310
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-05-15 20:11:54 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import { Header } from "./components/header";
+import { Hero } from "./components/section/hero";
+import { Usps } from "./components/section/usps";
+import { VideoCarousel } from "./components/section/video-carousel";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="bg-background relative z-10">
+          <Hero />
+          <Usps />
+        </div>
+        <VideoCarousel />
+        <div className="" />
+      </main>
+    </>
+  );
+}
+
+export default App;
